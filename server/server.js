6ce4fdb80edc04663ceab652f6168945081be85f
RyanADesigns <[ryan.stupey@gmail.com]>
2018-02-08 10:53:58 -0700

    added dotenv config to server file

--- server/server.js
+++ server/server.js
@@ -3,14 +3,14 @@ const cors = require('cors');
 const massive = require('massive');
 const bodyParser = require('body-parser')
 const ctrl = require('./controllers/index-controller.js')
-
+require('dotenv').config();
 const app = express();
 
 app.use(cors()); //<-- check postman to see if you need cors
 
 app.use(bodyParser.json());
 
-massive(process.env.DB_CONNECTION_STRING)
+massive(process.env.DB_CONNECTION_STRING) //<-- need to input connection string into .env file
 .then(dbInstance => app.set('db', dbInstance));
 
 const url = 'api/messages' //<-- define endpoint base url here
